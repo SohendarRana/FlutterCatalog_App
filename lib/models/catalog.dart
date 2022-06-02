2d17f08f821033648de7cd251cfc8506cfb75064
SohendarRana <sohendar1@gmail.com>
2022-06-02 22:26:03 +0530

    day23

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -5,11 +5,11 @@ class CatalogModel {
 
 //get item by id
 
-  static Item getById(int id) =>
+  Item getById(int id) =>
       items.firstWhere((element) => element.id == id, orElse: null);
 
 // get item by position
-  static Item getByPosition(int pos) => items[pos];
+  Item getByPosition(int pos) => items[pos];
 }
 
 class Item {
